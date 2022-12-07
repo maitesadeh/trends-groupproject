6aee4369487ff478703cf62a5ab4792c50b19d35
Uyen <umh4@cornell.edu>
2022-12-07 13:18:50 -0500

    Gallery with detail complete

--- finalproject/pages/gallery.tsx
+++ finalproject/pages/gallery.tsx
@@ -1,183 +1,255 @@
 import Link from "next/link";
 import Nav from "../public/UI/Nav";
-import styles from "../styles/Gallery.module.css"
+import styles from "../styles/Gallery.module.css";
 
 const gallery = () => {
-  return (
-    <section className={styles.main}>
-      <Nav></Nav>
-      <div className={styles.catalog}>
-      <Link href={"/detail"}>        
-        <div className={styles.card}>
-            <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-            <div className={styles.content}>
-            <div className={styles.like}>
-                <h2 className={styles.name}>Cascadilla Gorge</h2>
-                <button>Love</button>
-              </div>
-              <p className={styles.author}>by <strong>Urchin</strong></p>
-              <p className={styles.date}>Nov 13th</p>
-            </div>
-          </div>
-        </Link>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-          <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p className={styles.author}>by <strong>Urchin</strong></p>
-            <p className={styles.date}>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-          <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p className={styles.author}>by <strong>Urchin</strong></p>
-            <p className={styles.date}>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-          <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p className={styles.author}>by <strong>Urchin</strong></p>
-            <p className={styles.date}>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG"/>
-          <div className={styles.content}>
-            <div className={styles.like}>
-              <h2 className={styles.name}>Cascadilla Gorge</h2>
-              <button>Love</button>
-            </div>
-            <p>by <strong>Urchin</strong></p>
-            <p>Nov 13th</p>
-          </div>
-        </div>
-        
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.main}>
+			<Nav></Nav>
+			<div className={styles.catalog}>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+				<a href={"/detail"}>
+					<div className={styles.card}>
+						<img src="https://i0.wp.com/lh4.ggpht.com/_-rpMcGsgiFQ/SgG2Frp0d5I/AAAAAAAADMw/qSGvxSh-Z9M/s512/DSC_0036.JPG" />
+						<div className={styles.content}>
+							<div className={styles.like}>
+								<h2 className={styles.name}>Cascadilla Gorge</h2>
+								<button>Love</button>
+							</div>
+							<p className={styles.author}>
+								by <strong>Urchin</strong>
+							</p>
+							<p className={styles.date}>Nov 13th</p>
+						</div>
+					</div>
+				</a>
+			</div>
+		</section>
+	);
 };
 
 export default gallery;
