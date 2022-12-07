efb7781af9f1ead60c5e99c291eb515f14c12d55
maitesadeh <mns56@cornell.edu>
2022-12-07 13:32:59 -0500

    form

--- finalproject/pages/addtoform.tsx
+++ finalproject/pages/addtoform.tsx
@@ -1,12 +1,31 @@
 import Button from "../public/UI/Button";
 import { FormEventHandler, useState } from "react"
+import styles from "../styles/Favorites.module.css"
 
 const AddToForm = () => {
 
 // return <<input type="file" ></input>
 return <form>
-    <textarea></textarea>
-    <input type="file" ></input>
+    <div className={styles.faves}>
+        <h1>Add your photo here!</h1>
+    </div>
+    <div className={styles.form}>
+        <h2>Photographer</h2>
+        <textarea></textarea>
+    </div>
+
+    <div className={styles.form}>
+        <h2>Description</h2>
+        <textarea></textarea>
+    </div>
+
+    <div className={styles.form}>
+        <h2>Image</h2>
+    </div>
+    
+    <input type="file" className={styles.form}></input>
+    
+    <button className={styles.form} id={styles.submit}>Submit</button>
 </form>
 }
 
--- finalproject/pages/form.tsx
+++ finalproject/pages/form.tsx
@@ -6,13 +6,14 @@ import AddToForm from "./addtoform";
 const form = () => {
     return (
       <div>
-        <Nav></Nav>
-        <div className={styles.faves}>
+        {/* <div className={styles.faves}></div> */}
+          <Nav></Nav>
+        {/* <div className={styles.faves}>
           <h2>Photographer</h2>
           <h2>Description</h2>
-          <h2>Upload Picture</h2>
+          <h2>Upload Picture</h2> */}
           <AddToForm/>
-        </div>
+        {/* </div> */}
       </div>
     );
   };
