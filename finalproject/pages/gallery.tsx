3a2081add5969119369fc79decbaf6c331f055b2
Uyen <umh4@cornell.edu>
2022-12-06 22:46:19 -0500

    gallery first design

--- finalproject/pages/gallery.tsx
+++ finalproject/pages/gallery.tsx
@@ -1,10 +1,43 @@
 import Nav from "../public/UI/Nav";
+import styles from "../styles/Gallery.module.css"
 
 const gallery = () => {
   return (
-    <section>
-      <div>
-        <Nav/>
+    <section className={styles.main}>
+      <Nav></Nav>
+      <div className={styles.catalog}>
+        <div className={styles.card}>
+          <img src="/images/cascadilla.jpg"/>
+          <div className={styles.content}>
+            <h2 className={styles.name}>Cascadilla Gorge</h2>
+            <p className={styles.author}>by <strong>Urchin</strong></p>
+            <p className={styles.date}>Nov 13th</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/cascadilla.jpg"/>
+          <div className={styles.content}>
+            <h2 className={styles.name}>Cascadilla Gorge</h2>
+            <p className={styles.author}>by <strong>Urchin</strong></p>
+            <p className={styles.date}>Nov 13th</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/cascadilla.jpg"/>
+          <div className={styles.content}>
+            <h2 className={styles.name}>Cascadilla Gorge</h2>
+            <p className={styles.author}>by <strong>Urchin</strong></p>
+            <p className={styles.date}>Nov 13th</p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/cascadilla.jpg"/>
+          <div className={styles.content}>
+            <h2 className={styles.name}>Cascadilla Gorge</h2>
+            <p className={styles.author}>by <strong>Urchin</strong></p>
+            <p className={styles.date}>Nov 13th</p>
+          </div>
+        </div>
       </div>
     </section>
   );
