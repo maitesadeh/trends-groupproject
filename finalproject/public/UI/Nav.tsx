1d0359899c59dbae1fbddac80346396776592825
Desmond Ababio <dna35@cornell.edu>
2022-12-07 09:41:39 -0500

    changed inittials

--- finalproject/public/UI/Nav.tsx
+++ finalproject/public/UI/Nav.tsx
@@ -3,18 +3,18 @@ import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-    <nav className={styles.navbar}>    
-        <h1>
-          <Link href="/">JUD</Link>
-        </h1>
-        <div className="pages">
-          <div className="item">
-            <Link href="gallery"> Gallery</Link>
-          </div>
-          <div className="item">
-            <Link href="favorites">Favorites</Link>
-          </div>
+    <nav className={styles.navbar}>
+      <h1>
+        <Link href="/">MUD</Link>
+      </h1>
+      <div className="pages">
+        <div className="item">
+          <Link href="gallery"> Gallery</Link>
         </div>
+        <div className="item">
+          <Link href="favorites">Favorites</Link>
+        </div>
+      </div>
     </nav>
   );
 };
